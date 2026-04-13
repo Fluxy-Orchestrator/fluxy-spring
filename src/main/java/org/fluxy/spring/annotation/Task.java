88f067af90c00c73b435f86b209f2a7753d50679
Chronno <quinterosmanu@gmail.com>
2026-04-13 01:45:15 -0300

    Add Task annotation with runtime retention and type target

--- src/main/java/org/fluxy/spring/annotation/Task.java
+++ src/main/java/org/fluxy/spring/annotation/Task.java
@@ -3,8 +3,15 @@ package org.fluxy.spring.annotation;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Indexed;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 @Component
 @Indexed
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Task {
     String name();
     String description();
